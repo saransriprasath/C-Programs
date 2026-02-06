8c0a865b402ab691a85e5cf08125e4faf2f6751e
Saran Sri Prasath <saransriprasath.kiot@gmail.com>
2026-02-06 12:26:12 +0530

    Create Basic Arithmetic Operations.c

---
+++ Basic Arithmetic Operations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product: %d\n", a * b);
+    printf("Quotient: %d\n", a / b);
+    printf("Remainder: %d\n", a % b);
+
+    return 0;
+}
