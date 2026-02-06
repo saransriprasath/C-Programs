9f0cd14063a9e8baad91d5cbccb37a221def834f
Saran Sri Prasath <saransriprasath.kiot@gmail.com>
2026-02-06 12:32:52 +0530

    Create Prime Number or Not.c

---
+++ Prime Number or Not.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    int n, i, flag = 0;
+    scanf("%d", &n);
+
+    for(i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (n <= 1)
+        printf("Not Prime");
+    else if (flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
