4b90bbc0659bf3c0a1ad42ba7c2069399097aa15
Saran Sri Prasath <saransriprasath.kiot@gmail.com>
2026-02-06 12:31:37 +0530

    Create Leap Year Checker.c

---
+++ Leap Year Checker.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n%4==0 && n%100!=0 )
+        printf("Leap Year");
+    else if(n%400==0)
+        printf("Leap Year");
+    else
+        printf("Not a Leap Year");
+    return 0;
+}
